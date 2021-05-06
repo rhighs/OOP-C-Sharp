9ffed2528728a9b863614e943fa3bbbb5f994ebe
juri simoncini <juri.simoncini@studio.unibo.it>
2021-05-07 00:12:37 +0200

    Add Simoncini task

---
+++ CovSim_Simoncini/task_cSharp/Cov_sim/Infection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cov_sim
+{
+    public class Infection
+    {
+        private static readonly int STATUS_PERC = 70;
+        private static readonly int FP1_PERC = 30;
+        private static readonly int FP2_PERC = 20;
+        private static readonly int FP3_PERC = 10;
+
+        /**
+         * Run infection algorithm.
+         * This specific algorithm doesn't take care of the infector
+         * and only looks at the victim.
+         */
+        public bool apply(Person victim)
+        {
+            if (victim.isInfected())
+            {
+                return false;
+            }
+
+            int trasm = checkTrasmissibility(victim);
+            var rand = new Random();
+            int upperbound = 101;
+            return rand.Next(upperbound) <= trasm;
+        }
+
+        /**
+         * Check trasmissibility of a person.
+         * Trasmissibility only depends on the mask.
+         */
+        public int checkTrasmissibility(Person person)
+        {
+            var protection = person.getMask().getProtection();
+            var status = person.getMask().getStatus();
+            int perc = 0;
+
+            perc += status == Person.Mask.Status.UP ? 0 : STATUS_PERC;
+            switch (protection)
+            {
+                case Person.Mask.Protection.FP1:
+                    perc += FP1_PERC;
+                    break;
+                case Person.Mask.Protection.FP2:
+                    perc += FP2_PERC;
+                    break;
+                case Person.Mask.Protection.FP3:
+                    perc += FP3_PERC;
+                    break;
+            }
+
+            return perc;
+        }
+
+    }
+}
---
+++ CovSim_Simoncini/task_cSharp/Cov_sim/Person.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cov_sim
+{
+    public class Person
+    {
+        private bool infected = false;
+        private Mask mask;
+
+
+        public Person(Mask.Protection protection)
+        {
+
+            this.mask = new Mask(protection, Mask.Status.UP);
+        }
+
+        public bool isInfected()
+        {
+            return infected;
+        }
+
+        public Mask getMask()
+        {
+            return mask;
+        }
+
+        public void maskDown()
+        {
+            mask.setStatus(Mask.Status.DOWN);
+        }
+
+        public void switchMaskState()
+        {
+            mask.setStatus(mask.getStatus() == Mask.Status.UP ? Mask.Status.DOWN : Mask.Status.UP);
+        }
+
+        public class Mask
+        {
+            private Protection protection;
+            private Status status;
+
+            /**
+             * @param protection
+             * @param status
+             */
+            public Mask(Protection protection, Status status)
+            {
+                this.status = status;
+                this.protection = protection;
+            }
+
+            /**
+             * Returns the status of the mask.
+             */
+            public Status getStatus()
+            {
+                return status;
+            }
+
+            public void setStatus(Status status)
+            {
+                this.status = status;
+            }
+
+            /**
+             * Return the mask protection.
+             */
+            public Protection getProtection()
+            {
+                return protection;
+            }
+
+            public enum Status
+            {
+                UP,
+                DOWN
+            }
+
+            public enum Protection
+            {
+                FP1,
+                FP2,
+                FP3,
+            }
+        }
+    }
+
+}
